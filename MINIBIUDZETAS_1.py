375c0807750838f5684063d9c9b5bfe43419cfe9
Justas888 <justenas123@gmail.com>
2024-11-29 13:56:12 +0200

    Meniu, pasirinkimų įvedimas

---
+++ MINIBIUDZETAS_1.py
@@ -0,0 +1,26 @@
+pajamos = []
+islaidos = []
+
+while True:
+    print(" 1. Įveskite pajamas\n",
+          "2. Įveskite išlaidas\n",
+          "3. Atspausdinti pajamų eilutes\n",
+          "4. Atspausdinti išlaidų eilutes\n",
+          "5. Atspausdinti statistika\n",
+          "q - išeiti iš programos\n",
+
+          )
+    pasirinkimas = input(">> ")
+    if pasirinkimas == "q":
+        break
+    if pasirinkimas == "1":
+        pass
+
+    if pasirinkimas == "2":
+        pass
+
+    if pasirinkimas == "3":
+        pass
+
+print(f"Jūsų pajamos - {pajamos}")
+print(f"Jūsų išlaidos - {islaidos}")
